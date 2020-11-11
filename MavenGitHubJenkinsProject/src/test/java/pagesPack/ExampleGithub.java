8094be77aa94aff5ea128cd6a3e00805f9221222
shakh <shakh@DESKTOP-8IF7O58>
2020-11-11 15:33:24 -0500

    Example 11-11-2020 3:33

---
+++ MavenGitHubJenkinsProject/src/test/java/pagesPack/ExampleGithub.java
@@ -0,0 +1,11 @@
+package pagesPack;
+
+import org.testng.annotations.Test;
+
+public class ExampleGithub {
+	@Test
+	public void example() {
+		System.out.println("************************");
+	}
+
+}
